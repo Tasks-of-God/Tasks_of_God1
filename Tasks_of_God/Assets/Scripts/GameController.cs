e4267ca0f8e4ed850d57b53fb493ea7df561e40c
MYCOMPUTER\yuit0 <ogawatakafumi2814@gmail.com>
2019-12-24 18:32:56 +0900

    ファイル追加byたかし

---
+++ Tasks_of_God/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    InputField inputField;
+    Text text;
+
+    void Start()
+    {
+        inputField = GameObject.Find("InputField").GetComponent<InputField>();
+        text = GameObject.Find("Message").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void InputText()
+    {
+
+        text.text = inputField.text;
+
+    }
+
+}
